00ff5793c6b68e123d8627aad5b34100bc2d2512
ingridpitta <ingridpitta056@gmail.com>
2020-06-21 00:25:22 -0300

    SearchBar filter method

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,10 +4,11 @@ import { HomeContent } from "../../components/organisms";
 import "./home.css";
 
 class Home extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
+      data: this.props.data,
       title: "TechJobs",
       filters: [
         "Todos",
@@ -31,14 +32,37 @@ class Home extends Component {
     this.setState({ inputValue: value });
   };
 
-  onClick = () => {
-    console.log("clicked");
+  arrToLowerCase = (arr) => {
+    for (let i = 0 ; i < arr.length; i++){
+    for (let key in arr[i]){
+        if (key === undefined || arr[i][key] === undefined) {
+            continue;
+        }
+          const item = arr[i][key];
+          if(!Array.isArray(item)){
+            arr[i][key] = arr[i][key].toString().toLowerCase();
+          }
+        };
+    };
+  }
+
+  onClick = async () => {
+    const { inputValue } = this.state;
+    const {data} = this.props;
+
+    const mutatedData = Object.assign(data);
+
+    await this.arrToLowerCase(mutatedData);
+
+    const search = await mutatedData.filter(job => job.position.includes(inputValue.toLowerCase()));
+    
+    this.setState({data: search});
   };
 
 
   render() {
-    const { title, filters, inputValue } = this.state;
-    const {data, getJobInfos, loggedUser, logout } = this.props;
+    const { title, filters, inputValue, data } = this.state;
+    const {getJobInfos, loggedUser, logout } = this.props;
     console.log({ inputValue });
     return (
       <GeneralTemplate loggedUser={loggedUser} logout={logout} >
